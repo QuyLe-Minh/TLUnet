35a528ac2bff528fd2e513a71df79225f95e8ae2
Quy Le <leminhquytp@gmail.com>
2024-01-30 22:40:47 +0700

    fix loss func

--- loss/dice_loss.py
+++ loss/dice_loss.py
@@ -1,19 +1,26 @@
 import torch
 from torch import nn
 import numpy as np
-from utils import one_hot_encoder
 
 class Dice_loss(nn.Module):
     def __init__(self):
         super().__init__()
+        
+    def one_hot_encoder(self, pred):
+        mask = torch.zeros(pred.shape).to("cuda")
+        one_hot = pred.argmax(1)
+        mask[:, 0, :, :, :] = torch.where(one_hot==0, 1, 0)
+        mask[:, 1, :, :, :] = torch.where(one_hot==1, 1, 0)
+        
+        return mask
     
-    def forward(self, gt, pred):
+    def forward(self, pred, gt):
         """
         pred is unnormalized logits of shape (N, num_classes, H, W, D)
-        one_hot pased
         """
-        intersect = torch.sum(gt * pred)
-        union = torch.sum(gt + pred)
+        mask = self.one_hot_encoder(pred)
+        intersect = torch.sum(gt * mask)
+        union = torch.sum(gt + mask)
         eps = 1e-5
         dice = 2 * (intersect + eps)/(union + eps)
         return 1 - dice 
