eff2028090ec930cca22d641b1b46bfdb51181eb
Quy Le <leminhquytp@gmail.com>
2024-01-31 11:02:06 +0700

    Network baseline

--- architecture/TLUnet.py
+++ architecture/TLUnet.py
@@ -1,70 +1,34 @@
-import torch
-from torch import nn
-
-class ConvBlock(nn.Module):
-    def __init__(self, kernel, in_channels, out_channels):
-        super().__init__()
-        self.conv = nn.Sequential(
-            nn.Conv3d(in_channels, out_channels, kernel, 1, padding="same"),
-            nn.BatchNorm3d(out_channels),
-            nn.GELU()
-        )
-    
-    def forward(self, x):
-        output = self.conv(x)
-        return output
+from components import *
 
 class DeconvBlock(nn.Module):
     def __init__(self, in_channels, out_channels):
         super().__init__()
+        self.skip_conv = ConvBlock(5, in_channels, in_channels - out_channels)
         self.conv1 = ConvBlock(3, in_channels, in_channels)
         self.conv2 = ConvBlock(3, in_channels, in_channels)
         self.deconv = nn.ConvTranspose3d(in_channels, out_channels, 2, stride = 2)
         
-    def forward(self, expansive, contractive):
-        fusion = torch.cat([expansive, contractive], dim = 1)
-        up = self.conv1(fusion)
-        up = self.conv2(up)
-        up = self.deconv(up)
-        return up
-    
-class EncoderBlock(nn.Module):
-    def __init__(self, skip_channels, in_channels, out_channels, is_last = False):
-        super().__init__()
-        self.is_last = is_last
-        if self.is_last:
-            self.conv1 = ConvBlock(3, in_channels, out_channels)
-            self.skip_conv = ConvBlock(5, out_channels, skip_channels)
-        else:
-            self.conv1 = ConvBlock(3, in_channels, out_channels//2)
-            self.skip_conv = ConvBlock(5, out_channels//2, skip_channels)
-            self.conv2 = ConvBlock(3, out_channels//2, out_channels)
-        self.pool = nn.MaxPool3d(2, stride = 2)
-    
-    def forward(self, x):
+    def forward(self, x, skip):
         x = self.conv1(x)
-        skip = self.skip_conv(x)
-        x = self.pool(x)
-        if not self.is_last:
-            x = self.conv2(x)
-        
-        return x, skip
+        x = self.conv2(x)
+        ds = x
+        x = self.deconv(x)
+        skip = self.skip_conv(skip)
+        up = torch.cat([x, skip], dim = 1)
+        return up, ds
+
         
 class TLUnet(nn.Module):
     def __init__(self, n_classes = 2):
         super().__init__()
         filters = 32
         n_deconv = 96
-        self.eblock1 = EncoderBlock(4, 1, filters)
-        self.eblock2 = EncoderBlock(8, filters, filters * 2)
-        self.eblock3 = EncoderBlock(16, filters * 2, filters * 4)
-        self.eblock4 = EncoderBlock(32, filters * 4, filters * 4, is_last=True)
+        self.eblock1 = EncoderBlock(1, filters)
+        self.eblock2 = EncoderBlock(filters, filters * 2)
+        self.eblock3 = EncoderBlock(filters * 2, filters * 4)
+        self.eblock4 = EncoderBlock(filters * 4, filters * 8, is_last=True)
         
-        self.bottle_neck = nn.Sequential(
-            ConvBlock(3, filters * 4, filters * 8),
-            ConvBlock(3, filters * 8, filters * 8),
-            nn.ConvTranspose3d(filters * 8, n_deconv, 2, 2)
-        )
+        self.deconv0 = nn.ConvTranspose3d(filters * 8, n_deconv, 2, 2)
         
         self.deconv1 = DeconvBlock(filters * 4, n_deconv//2)
         self.deconv2 = DeconvBlock(filters * 2, n_deconv//4)
@@ -76,24 +40,24 @@ class TLUnet(nn.Module):
             nn.Conv3d(filters//2, n_classes, 1)
         )
         
-        self.ds1 = nn.Conv3d(n_deconv//2, n_classes, 1)
-        self.ds2 = nn.Conv3d(n_deconv//4, n_classes, 1)
-        self.ds3 = nn.Conv3d(n_deconv//8, n_classes, 1)  
+        self.ds2 = nn.Conv3d(filters*2, n_classes, 1)
+        self.ds3 = nn.Conv3d(filters, n_classes, 1)  
     def forward(self, input):
         x1, skip1 = self.eblock1(input) #96
         x2, skip2 = self.eblock2(x1) #48
         x3, skip3 = self.eblock3(x2) #24
         x4, skip4 = self.eblock4(x3) #12
         
-        x4 = self.bottle_neck(x4) #24 
+        x4 = self.deconv0(x4) #24
         
-        d1 = self.deconv1(x4, skip4) #48
-        ds1 = self.ds1(d1)
-        d2 = self.deconv2(d1, skip3) #96
-        ds2 = self.ds2(d2)
-        d3 = self.deconv3(d2, skip2) #192
+        concat = torch.cat([x4, skip4], dim = 1) 
         
-        d4 = torch.cat((d3, skip1), dim = 1)
-        res = self.head(d4)
+        d1, _ = self.deconv1(concat, skip3) 
+        d2, ds2 = self.deconv2(d1, skip2) 
+        d3, ds3 = self.deconv3(d2, skip1)
         
-        return res, ds1, ds2
+        ds2 = self.ds2(ds2)
+        ds3 = self.ds3(ds3) 
+        pred = self.head(d3)
+        
+        return pred, ds2, ds3
