480e67fcb70b2c227b1b19a5989d6c874cd1d8d7
Quy Le <leminhquytp@gmail.com>
2024-02-18 19:55:25 +0700

    optim

--- utils.py
+++ utils.py
@@ -4,14 +4,16 @@ import os
 
 class Config:
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-    batch_size = 5
+    batch_size = 7
     epochs = 1000
     n_classes = 2
-    patience = 3
+    patience = 10
     n_freeze = 36
-    train = [f"dataset/train/{file}" for file in os.listdir("dataset/train")]
-    val = [f"dataset/val/{file}" for file in os.listdir("dataset/val")]
-    mode = "training"
+    train = [f"dataset/train_tlu/{file}" for file in os.listdir("dataset/train_tlu")]
+    val = [f"dataset/val_tlu/{file}" for file in os.listdir("dataset/val_tlu")]
+    mode = "tuning"
+
+config = Config()
 
 def one_hot_encoder(input, n_classes=2):
     """One hot encode
@@ -29,7 +31,6 @@ def one_hot_encoder(input, n_classes=2):
     one_hot[:, 1, :, :, :] = torch.where(tmp == 1, 1, 0)
     
     return one_hot.to(torch.device("cuda" if torch.cuda.is_available() else "cpu"))
-      
       
 def manual_crop(X):
     X_cropped_collection = []
@@ -49,17 +50,27 @@ def manual_crop(X):
                     start_k = d - 64
                 else:
                     start_k = k*64
-                X_cropped = X[:, :, start_i : start_i+192, start_j : start_j+192, start_k : start_k+64]
-                X_cropped_collection.append(X_cropped.to(torch.device("cuda" if torch.cuda.is_available() else "cpu")))
+                X_cropped = X_cropped = X[0, :, start_i : start_i+192, start_j : start_j+192, start_k : start_k+64]
+                X_cropped_collection.append(X_cropped)
                 
-    return X_cropped_collection
+    n_cubes = len(X_cropped_collection)
+    batches = []
+    for i in range(ceil(n_cubes/config.batch_size)):
+        if (i+1)*config.batch_size > n_cubes:
+            batch = torch.stack(X_cropped_collection[i*config.batch_size:], dim = 0).cuda()
+            batches.append(batch)
+        else:
+            batch = torch.stack(X_cropped_collection[i*config.batch_size:(i+1)*config.batch_size], dim = 0).cuda()
+            batches.append(batch)
+                
+    return batches
 
 def concat(y, y_cropped_collection):
     """Concat pieces of cube
 
     Args:
-        y (ground truth): b=1, c, 512, 512, d
-        y_cropped_collection (pred): collection of cubes: n, b_, c_, 192, 192, 64
+        y (ground truth): b=1, c = 1, 512, 512, d
+        y_cropped_collection (pred): list of batches: n, b_, c_, 192, 192, 64
     """
     b, c, h, w, d = y.shape
     pred = torch.empty(y.shape)
@@ -79,7 +90,7 @@ def concat(y, y_cropped_collection):
                     start_k = d - 64
                 else:
                     start_k = k*64
-                pred[:, :, start_i:start_i+192, start_j:start_j+192, start_k:start_k+64] = y_cropped_collection[sample]
+                pred[:, :, start_i:start_i+192, start_j:start_j+192, start_k:start_k+64] = y_cropped_collection[sample//config.batch_size][sample%config.batch_size]
                 sample += 1
                 
-    return pred.to(torch.device("cuda" if torch.cuda.is_available() else "cpu"))
+    return pred.cuda()
