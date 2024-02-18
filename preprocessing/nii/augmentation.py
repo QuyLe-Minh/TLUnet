3a06e8129ff663ecc4857f04e878fe5b21b76238
Quy Le <leminhquytp@gmail.com>
2024-02-18 10:58:47 +0700

    big update

--- preprocessing/nii/augmentation.py
+++ preprocessing/nii/augmentation.py
@@ -3,7 +3,7 @@ from preprocessing.nii.voxelization import *
 
 def augmented(image, label):
     transform = Compose([
-        RandRotated(keys=["image", "label"], range_x=10, range_y=10, range_z=45, prob=0.5),
+        RandRotated(keys=["image", "label"], range_x=np.pi/18, range_y=np.pi/18, range_z=np.pi/4, prob=0.5),
         Rand3DElasticd(keys=["image", "label"], sigma_range=(5, 7), magnitude_range=(50, 150), prob=0.5),
         RandSpatialCropSamplesd(keys=["image", "label"], num_samples=60, roi_size=(64, 192, 192)),
         ToTensor(),
