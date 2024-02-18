c392bcef6013c80f29843a59768498217df3822a
Quy Le <leminhquytp@gmail.com>
2024-02-18 20:13:26 +0700

    apply pretrained

--- train.py
+++ train.py
@@ -1,8 +1,42 @@
 from monai.losses import DiceLoss
-from val import *
 from architecture.TLUnet import TLUnet
 from torch.optim.lr_scheduler import ReduceLROnPlateau
 from torch import nn
+import torch
+from utils import manual_crop, concat
+from metrics.metrics import *
+
+def val(config, dataloader, model, entropy_loss, dice_loss):
+    size = len(dataloader.dataset)
+    num_batches = len(dataloader)
+    test_loss, correct = 0, 0
+    dice_score_liver, iou_score_liver = 0, 0
+
+    with torch.no_grad():
+        for X, y in dataloader:
+            y = y.to(torch.float).to(config.device)
+            
+            X_cropped_collection = manual_crop(X)
+            y_cropped_collection = []
+            for i in range(len(X_cropped_collection)):
+                y_cropped = model(X_cropped_collection[i])
+                y_cropped_collection.append(y_cropped.detach().cpu())
+            
+            pred = concat(y, y_cropped_collection)
+            
+            test_loss += dice_loss(pred, y)
+            correct += (torch.round(pred).to(torch.uint8) == torch.round(y).to(torch.uint8)).type(torch.float).mean().item()
+
+            dice_score_liver += dice(pred, y)
+            iou_score_liver += iou(pred, y)
+
+    test_loss /= num_batches
+    correct /= size
+    dice_score_liver /= size
+    iou_score_liver /= size
+    print(f"Test Error: \n Accuracy: {(100*correct):>0.1f}%, Avg loss: {test_loss:>8f} \n")
+    print(f"Dice score liver: {(100 * dice_score_liver):>0.3f}%, IoU score liver: {(iou_score_liver * 100):>0.3f}% \n")
+    return test_loss
 
 def train(config, dataloader, model, entropy_loss, dice_loss, optimizer):
     size = len(dataloader.dataset)
@@ -17,11 +51,11 @@ def train(config, dataloader, model, entropy_loss, dice_loss, optimizer):
         # forward + backward + optimize
         pred = model(X) #output, ds1, ds2, ds3 (least to most)
         
-        loss = entropy_loss(pred[0], y) * 8 + entropy_loss(pred[1], y) * 1 + entropy_loss(pred[2], y) * 2 + entropy_loss(pred[3], y) * 4 + dice_loss(pred[0], y)
+        loss = entropy_loss(pred, y) * 0.4 + 0.6 * dice_loss(pred, y)
         loss.backward()
         optimizer.step()
 
-        correct += (torch.round(pred[0]).to(torch.uint8) == torch.round(y).to(torch.uint8)).type(torch.float).mean().item()
+        correct += (torch.round(pred).to(torch.uint8) == torch.round(y).to(torch.uint8)).type(torch.float).mean().item()
 
         if batch % 100 == 0:
             loss, current = loss.item(), batch * len(X)
