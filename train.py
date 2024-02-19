1f6c75da4319f104f000eaef22764ef4baf52167
leminhquy <leminhquytp@gmail.com>
2024-02-19 15:00:07 +0700

    optim

--- train.py
+++ train.py
@@ -1,6 +1,6 @@
 from monai.losses import DiceLoss
 from architecture.CNN3D import CNN3D
-from torch.optim.lr_scheduler import ReduceLROnPlateau
+from torch.optim.lr_scheduler import ReduceLROnPlateau, StepLR
 from torch import nn
 import torch
 from utils import manual_crop, concat
@@ -20,7 +20,7 @@ def train(config, dataloader, model, entropy_loss, dice_loss, optimizer):
         # forward + backward + optimize
         pred = model(X) #output, ds1, ds2, ds3 (least to most)
         
-        loss = 0.4 * (entropy_loss(pred[0], y) * 8 + entropy_loss(pred[1], y) * 1 + entropy_loss(pred[2], y) * 2 + entropy_loss(pred[3], y) * 4) + 0.6 * dice_loss(pred[0], y)
+        loss = entropy_loss(pred[0], y) * 8 + entropy_loss(pred[1], y) * 1 + entropy_loss(pred[2], y) * 2 + entropy_loss(pred[3], y) * 4 + dice_loss(pred[0], y)
         loss.backward()
         optimizer.step()
 
@@ -64,8 +64,7 @@ def val(config, dataloader, model, dice_loss):
     iou_score_liver /= size
     print(f"Dice score liver: {(100 * dice_score_liver):>0.3f}%, IoU score liver: {(iou_score_liver * 100):>0.3f}% \n")
     return test_loss
-    
-    
+        
 def training(config, train_loader, val_loader, mode):
     model = CNN3D().to(config.device)
     if mode != "training":
@@ -73,21 +72,25 @@ def training(config, train_loader, val_loader, mode):
         model.load_state_dict(torch.load(path))
         print(f"Load model {path}...")
     model.train()
-
+    
+    # entropy_loss = nn.CrossEntropyLoss()
     entropy_loss = nn.BCELoss()
     dice_loss = DiceLoss(squared_pred = True)
-    optimizer = torch.optim.Adam(model.parameters(), lr=1e-5, weight_decay=1e-3) #3e-5
-    scheduler = ReduceLROnPlateau(optimizer, 'min')
+    optimizer = torch.optim.Adam(model.parameters(), lr=1e-4, weight_decay=1e-4) #3e-5
+    # scheduler = ReduceLROnPlateau(optimizer, 'min')
+    scheduler = StepLR(optimizer, step_size=30, gamma=1/3)
     
     torch.cuda.empty_cache()
     best_one = 0.086234
+    count = 0
     
     for t in range(config.epochs):
         print(f"Epoch {t+1}\n-------------------------------")
         train(config, train_loader, model, entropy_loss, dice_loss, optimizer)
-        val_loss = val(config, val_loader, model, dice_loss)
+        val_loss = val(config, val_loader, model, entropy_loss, dice_loss)
 
         if val_loss < best_one:
+            count = 0
             best_one = val_loss
             torch.save(model.state_dict(), "cnn3d_3.pt")
         torch.save(model.state_dict(), "cnn3d.pt")
@@ -97,4 +100,4 @@ def training(config, train_loader, val_loader, mode):
         #     if count == config.patience:
         #         break
 
-        # scheduler.step(val_loss)
+        scheduler.step()
