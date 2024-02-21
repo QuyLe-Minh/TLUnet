7e05c3ed64446df23a1dc9dc6bf68af6b98617a4
leminhquy <leminhquytp@gmail.com>
2024-02-21 10:28:10 +0700

    config

--- train.py
+++ train.py
@@ -1,6 +1,6 @@
 from monai.losses import DiceLoss
 from architecture.TLUnet import TLUnet
-from torch.optim.lr_scheduler import ReduceLROnPlateau
+from torch.optim.lr_scheduler import ReduceLROnPlateau, StepLR
 from torch import nn
 import torch
 from utils import manual_crop, concat
@@ -84,15 +84,17 @@ def training(config, train_loader, val_loader, mode):
         path = "tlu.pt" 
         model.load_state_dict(torch.load(path))
         print(f"Load model {path}...")
-        
-    applyWeight(config, model, config.model_weight)
+
+    else: applyWeight(config, model, config.model_weight)
+
     model.train()
     
     # entropy_loss = nn.CrossEntropyLoss()
     entropy_loss = nn.BCELoss()
     dice_loss = DiceLoss(squared_pred = True)
     optimizer = torch.optim.Adam(model.parameters(), lr=1e-4, weight_decay=1e-4) #3e-5
-    scheduler = ReduceLROnPlateau(optimizer, 'min')
+    # scheduler = ReduceLROnPlateau(optimizer, 'min')
+    scheduler = StepLR(optimizer, step_size=30, gamma=1/3)
     
     torch.cuda.empty_cache()
     best_one = 100
@@ -104,12 +106,12 @@ def training(config, train_loader, val_loader, mode):
 
         if val_loss < best_one:
             best_one = val_loss
-            torch.save(model.state_dict(), "cnn3d_3.pt")
-        torch.save(model.state_dict(), "cnn3d.pt")
+            torch.save(model.state_dict(), "best_tlu.pt")
+        torch.save(model.state_dict(), "tlu.pt")
         # else:
         #     # model.load_state_dict(torch.load("cnn3d_2.pt"))
         #     count+=1
         #     if count == config.patience:
         #         break
 
-        # scheduler.step(val_loss)
+        scheduler.step()
