ac2445308c376a8eb6ddf787b40c0aaba5ebd6cc
Quy Le <leminhquytp@gmail.com>
2024-09-02 21:14:43 +0700

    feat: loss for adv

--- osnet/training/loss_functions/dice_loss.py
+++ osnet/training/loss_functions/dice_loss.py
@@ -372,6 +372,71 @@ class DC_and_Focal_loss(nn.Module):
         
         return result
     
+class DC_and_Focal_and_Adv_loss(nn.Module):
+    def __init__(self, soft_dice_kwargs, ce_kwargs, aggregate="sum", square_dice=False, weight_ce=1, weight_dice=1,
+                 log_dice=False, ignore_label=None):
+        """
+        CAREFUL. Weights for CE and Dice do not need to sum to one. You can set whatever you want.
+        :param soft_dice_kwargs:
+        :param ce_kwargs:
+        :param aggregate:
+        :param square_dice:
+        :param weight_ce:
+        :param weight_dice:
+        """
+        super(DC_and_Focal_and_Adv_loss, self).__init__()
+        if ignore_label is not None:
+            assert not square_dice, 'not implemented'
+            ce_kwargs['reduction'] = 'none'
+        self.log_dice = log_dice
+        self.weight_dice = weight_dice
+        self.weight_ce = weight_ce
+        self.aggregate = aggregate
+        self.focal = FocalLoss(to_onehot_y=True, 
+                               use_softmax=True, 
+                               )
+
+        self.ignore_label = ignore_label
+
+        if not square_dice:
+            self.dc = SoftDiceLoss(apply_nonlin=softmax_helper, **soft_dice_kwargs)
+        else:
+            self.dc = SoftDiceLossSquared(apply_nonlin=softmax_helper, **soft_dice_kwargs)
+
+    def forward(self, net_output, target, reconstruct, origin_img):
+        """
+        target must be b, c, x, y(, z) with c=1
+        :param net_output:
+        :param target:
+        :return:
+        """
+        if self.ignore_label is not None:
+            assert target.shape[1] == 1, 'not implemented for one hot encoding'
+            mask = target != self.ignore_label
+            target[~mask] = 0
+            mask = mask.float()
+        else:
+            mask = None
+
+        dc_loss = self.dc(net_output, target, loss_mask=mask) if self.weight_dice != 0 else 0
+        if self.log_dice:
+            dc_loss = -torch.log(-dc_loss)
+
+        focal_loss = self.focal(net_output, target) if self.weight_ce != 0 else 0
+        if self.ignore_label is not None:
+            focal_loss *= mask[:, 0]
+            focal_loss = focal_loss.sum() / mask.sum()
+
+        if self.aggregate == "sum":
+            result = self.weight_ce * focal_loss + self.weight_dice * dc_loss
+        else:
+            raise NotImplementedError("nah son") # reserved for other stuff (later)
+        
+        adv_loss = 1 - (reconstruct - origin_img) ** 2
+        adv_loss = torch.where(adv_loss < 0.75, torch.zeros_like(adv_loss), adv_loss)
+        adv_loss = adv_loss.mean()
+        return result + adv_loss
+    
 class DC_and_CE_loss(nn.Module):
     def __init__(self, soft_dice_kwargs, ce_kwargs, aggregate="sum", square_dice=False, weight_ce=1, weight_dice=1,
                  log_dice=False, ignore_label=None, weight = None):
